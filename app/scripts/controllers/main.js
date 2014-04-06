ebe624c94064bc4e0f543bbd5aa97877594ca67a
Anabelle Handdoek <github@huellaspyp.com>
2014-04-06 12:23:58 -0500

    adds hardcoded scope and filter

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -2,9 +2,23 @@
 
 angular.module('permitidorayarApp')
   .controller('MainCtrl', function ($scope) {
-    $scope.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
+    $scope.mensajes = [
+		{
+			'fecha' : '03/04/2014',
+			'texto': 'Mensaje 1 Max 140 Caracteres.',
+			'autor': 'Firma 1 25 Cars'
+		},
+
+		{
+			'fecha' : '03/04/2014',
+			'texto': 'Mensaje 2 Max 140 Caracteres.',
+			'autor': 'Firma 2 25 Cars'
+		},
+
+		{
+			'fecha' : '03/04/2014',
+			'texto': 'Mensaje 3 Max 140 Caracteres.',
+			'autor': 'Firma 3 25 Cars'
+		}
     ];
   });
