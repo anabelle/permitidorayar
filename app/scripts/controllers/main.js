d2bd6184bc19c50d9351cea35a015a73cc42f1c4
decolector <cmart@decolector.net>
2014-04-10 13:29:39 -0300

    angular refresh

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -3,16 +3,19 @@
 angular.module('permitidorayarApp')
   .controller('MainCtrl', function ($scope, $http) {
 
-  //$.param($scope.formData),  // pass in data as strings
-
   $scope.datetime = {time : new Date()};
 
- $http({method:'GET', url: 'https://api.mongohq.com/databases/vital/collections/messages/documents?_apikey=6pnomhzb6yre2nifkc4u&sort={"date":-1}&limit=10'})
-    .success(function(data, status, headers, config){
-      console.log(data);
-    $scope.mensajes = data;
-  });
 
+
+  $scope.pullData = function(){
+    $http({method:'GET', url: 'https://api.mongohq.com/databases/vital/collections/messages/documents?_apikey=6pnomhzb6yre2nifkc4u&sort={"date":-1}&limit=10'})
+      .success(function(data, status, headers, config){
+        console.log(data);
+        $scope.mensajes = data;
+      });
+  };
+
+  $scope.pullData();
 
 
   $scope.processForm = function(){
@@ -21,8 +24,11 @@ angular.module('permitidorayarApp')
     $scope.formData.date = Date.now()
 
     var doc = JSON.stringify({'document':$scope.formData}, {"_id": -1});
-
-    //var doc = '{"document":{"author":"cmart", "body":"hola mundo"}}';
-    $http({method:'POST', url: 'https://api.mongohq.com/databases/vital/collections/messages/documents?_apikey=6pnomhzb6yre2nifkc4u', data:doc});
+    $http({method:'POST', url: 'https://api.mongohq.com/databases/vital/collections/messages/documents?_apikey=6pnomhzb6yre2nifkc4u', data:doc})
+      .success(function(data, status, headers, config){
+        console.log("success in save: " + data);
+        $scope.pullData();
+        $scope.$apply();
+      });
   }
 });
